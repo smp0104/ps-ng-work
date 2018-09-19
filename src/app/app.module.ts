e63a5a526d35293d47dfd941a451064b68a7b88c
Ku <Manikanta_Praveen_Ku@dell.com>
2018-09-20 03:41:55 +0530

    initial routing changes and added temporary home files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { FormsModule} from '@angular/forms';
 import {HttpClientModule } from '@angular/common/http';
-
+import {RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { ConvertToSpacePipe } from './shared/converttoSpace';
 import { StarComponent } from './star/star.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
     ConvertToSpacePipe,
-    StarComponent
+    StarComponent,
+    HomeComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'products/pd', component:ProductDetailComponent},
+      {path:'products', component:ProductComponent},
+      {path:'home', component:HomeComponent},
+      {path:'', redirectTo:'home', pathMatch:'full' },
+      {path:'**',redirectTo:'home', pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
